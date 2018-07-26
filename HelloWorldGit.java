dd9063f95e9c89d7df0b86cb4d9b1d369b1efdad
Shefali Gaonker <shefali_gaonker@persistent.co.in>
2018-07-26 15:38:09 +0530

    'git checkout -b idea' added an date displayed

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -3,15 +3,15 @@ Class HelloWorld
 
 	static void displayDate()
 	{
-		System.out.println("Display today's date:-");
+		System.out.println("Display today's date:-"+new Date());
 	}
 
 
 	public Static void main(String[] args)
 	{
-		System.out.println("Hello World.....Welcome to GIT");
+		System.out.println("Hello World.....!!! Welcome to GIT....!");
 
-		displayDate());
+		displayDate();
 		
 	}
 
